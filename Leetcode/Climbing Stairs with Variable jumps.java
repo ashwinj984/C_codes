8794bfd671a4420b1da06a9a3bb9707ee15c203e
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-09-10 14:30:40 +0530

    by Tabulation

---
+++ Leetcode/Climbing Stairs with Variable jumps.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] ab = new int[n];
+        for(int i = 0; i < n; i++){
+            ab[i] = scn.nextInt();
+        }
+        
+        int[] dp = new int[n+1];
+        
+        dp[n] = 1;
+        for(int i = n-1; i>= 0; i--){
+            for(int j = 1; j <= ab[i] && i + j < dp.length;j++){
+                dp[i] += dp[i+j]; 
+            }
+        }
+        System.out.println(dp[0]);
+    }
+
+}
