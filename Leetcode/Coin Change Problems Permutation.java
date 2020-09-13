1c4314f0691fda844ee33fabecbbd91b778495b7
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-09-14 00:45:56 +0530

    Create Coin Change Problems Permutation.java

---
+++ Leetcode/Coin Change Problems Permutation.java
@@ -0,0 +1,29 @@
+//Dyanamic programming
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+            Scanner scn = new Scanner(System.in);
+            
+            int n = scn.nextInt();
+            
+            int[] arr = new int[n];
+            
+            for(int i = 0; i < arr.length; i++){
+                arr[i] = scn.nextInt();
+            }
+            int amt = scn.nextInt();
+            
+            int[] dp = new int[amt + 1];
+            dp[0] = 1;
+            for(int i = 1; i  < dp.length; i++){
+                for(int j = 0; j < arr.length; j++){
+                    if( arr[j] <= i)
+                    dp[i] += dp[i - arr[j]];
+                }
+            }
+            System.out.println(dp[amt]);
+    }
+}
