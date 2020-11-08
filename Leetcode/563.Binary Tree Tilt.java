05489837b9b3f0570b2e523e47582ea24b635d43
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-08 23:24:41 +0530

    Create 563.Binary Tree Tilt.java

---
+++ Leetcode/563.Binary Tree Tilt.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTilt(TreeNode root) {
+          tilt(root);
+        return tilt;
+    }
+    
+    
+    
+     int tilt = 0;
+
+    private  int tilt(TreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+
+		int ls = tilt(node.left);
+		int rs = tilt(node.right);
+
+		int ltilt = Math.abs(ls - rs);
+		tilt += ltilt;
+		return ls + rs + node.val;
+	}
+    
+    
+}
