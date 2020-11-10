61bdfa0dd89d3f06b33d82951e9369e156fc3fb5
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-10 14:47:08 +0530

    Update 832.Flipping an Image.java

--- Leetcode/832.Flipping an Image.java
+++ Leetcode/832.Flipping an Image.java
@@ -53,4 +53,27 @@ class Solution {
         return A;
     }
 }
+
+
+
+
+More Concise Form Use of Ternary Operator
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for(int i=0;i<A.length;i++){
+            int start=0;
+            int end = A[i].length-1;
+            while(start<=end){
+                int temp = (A[i][start] > 0 ? 0: 1);
+                A[i][start] = (A[i][end] > 0 ? 0: 1);
+                A[i][end] =temp;
+                
+                end--;start++;
+            }
+        }
+        
+        return A;
+    }
+}
     
