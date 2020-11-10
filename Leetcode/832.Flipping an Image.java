6f505bc32d0a5cdfeec8233839031340075e0797
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-10 14:43:33 +0530

    100% faster version added

--- Leetcode/832.Flipping an Image.java
+++ Leetcode/832.Flipping an Image.java
@@ -20,3 +20,37 @@ class Solution {
         return b;
     }
 }
+
+
+//100% faster
+        |
+        |
+        |
+            
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        
+        int n = A[0].length - 1;
+        for(int i  = 0; i < A.length; i++){
+            for(int j = 0; j < A[0].length;j++){
+                int temp = A[i][j];
+                 A[i][j] = A[i][n - j];
+                A[i][n - j] = temp;
+                if(j >= n/2){
+                    break;
+                }
+            }
+        }
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j <= n; j++){
+                if(A[i][j] == 0){
+                    A[i][j] = 1;
+                }else{
+                    A[i][j] = 0;
+                }
+            }
+        }
+        return A;
+    }
+}
+    
