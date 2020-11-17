9b1640c34c22656de324e9d9f36aebf26c019eff
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-17 19:04:58 +0530

    another method is added

--- Leetcode/858.Mirror Reflection.java
+++ Leetcode/858.Mirror Reflection.java
@@ -11,3 +11,18 @@ class Solution {
         return -1;
     }
 }
+
+
+//One more simple approach
+
+ public int mirrorReflection(int p, int q) {
+        int m = 1, n = 1;
+        while(m * p != n * q){
+            n++;
+            m = n * q / p;
+        }
+        if (m % 2 == 0 && n % 2 == 1) return 0;
+        if (m % 2 == 1 && n % 2 == 1) return 1;
+        if (m % 2 == 1 && n % 2 == 0) return 2;
+        return -1;
+    }
