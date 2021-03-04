d14d31faa08b9a58cacc88a56b30db3e4016ce33
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-03-04 16:57:55 +0530

    Update 160.Intersection of Linked List.java

--- Leetcode/160.Intersection of Linked List.java
+++ Leetcode/160.Intersection of Linked List.java
@@ -21,3 +21,44 @@ public class Solution {
 
 //Best way to understand this problem is to debug it 
 // You will get a pattern and just code according to that
+
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+       ListNode one = headA;
+        ListNode two = headB;
+        
+        int len1 = 0;
+        int len2 = 0;
+        
+        while(one != null){
+            one = one.next;
+            len1++;
+        }
+        while(two != null){
+            two = two.next;
+            len2++;
+        }
+        
+        one = headA;
+        two = headB;
+        int delta = Math.abs(len1 - len2);
+        
+        if(len1 > len2){
+            for(int i = 0; i < delta; i++){
+                one = one.next;
+            }
+        }else{
+            for(int i = 0; i < delta; i++){
+                two = two.next;
+            }
+        }
+        
+        while(one != two){
+            one = one.next;
+            two = two.next;
+        }
+        
+        return one;
+    }
+}
