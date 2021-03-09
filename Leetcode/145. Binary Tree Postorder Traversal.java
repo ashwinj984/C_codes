5160190b87b7d27cce6311df785d3567a2943f6b
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-03-09 17:11:32 +0530

    Create 145. Binary Tree Postorder Traversal.java

---
+++ Leetcode/145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+         List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+    public void helper(TreeNode root, List<Integer> res){
+        if(root == null){
+            return;
+        }
+        
+        helper(root.left,res);
+        helper(root.right,res);
+        res.add(root.val);
+    }
+}
