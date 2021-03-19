92f77a46e3338e2878c6302ba9dee2397f360219
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-03-19 20:39:09 +0530

    Create 417. Pacific Atlantic Water Flow.java

---
+++ Leetcode/417. Pacific Atlantic Water Flow.java
@@ -0,0 +1,53 @@
+class Solution{
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        if (matrix.length == 0)
+            return res;
+
+        int m = matrix.length, n = matrix[0].length;
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(i, 0, matrix, pacific, m, n);
+            dfs(i, n - 1, matrix, atlantic, m, n);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dfs(0, i, matrix, pacific, m, n);
+            dfs(m - 1, i, matrix, atlantic, m, n);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j])
+                    res.add(new ArrayList<Integer>(Arrays.asList(i, j)));
+            }
+        }
+
+        return res;
+    }
+
+    // O(m*n)
+    public void dfs(int x, int y, int[][] matrix, boolean[][] visited, int m, int n) {
+
+        visited[x][y] = true;
+        int val = matrix[x][y];
+
+        if (x - 1 >= 0 && !visited[x - 1][y] && matrix[x - 1][y] >= val)
+            dfs(x - 1, y, matrix, visited, m, n);
+
+        if (x + 1 < m && !visited[x + 1][y] && matrix[x + 1][y] >= val)
+            dfs(x + 1, y, matrix, visited, m, n);
+
+        if (y - 1 >= 0 && !visited[x][y - 1] && matrix[x][y - 1] >= val)
+            dfs(x, y - 1, matrix, visited, m, n);
+
+        if (y + 1 < n && !visited[x][y + 1] && matrix[x][y + 1] >= val)
+            dfs(x, y + 1, matrix, visited, m, n);
+
+    }
+}
