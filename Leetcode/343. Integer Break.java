4ad7347244d28319425cb0e38cd9427bcbea3150
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-07-22 20:52:13 +0530

    Update 343. Integer Break.java

--- Leetcode/343. Integer Break.java
+++ Leetcode/343. Integer Break.java
@@ -34,3 +34,40 @@ public class Solution {
 	}
 
 }
+
+// Another Approach
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+		for(int i = 1; i < dp.length; i++) {
+			for(int j = 1; j <= (int) Math.ceil(i / 2.0); j++) {
+				dp[i] = Math.max(dp[i], dp[i - j] * j);
+			}
+			
+			dp[i] = i != n ? Math.max(dp[i], i) : dp[i];
+		}
+        return dp[n];
+    }
+}
+// BEST APPROACH 
+class Solution {
+  public int integerBreak(int n) {
+    Integer[] dp = new Integer[n];
+    return explore(n, 0, dp);
+  }
+  
+  public int explore(int n, int sum, Integer[] dp) {
+    if(sum > n) return 0;
+    if(sum == n) return 1;
+    if(dp[sum] != null) return dp[sum];
+    
+    int max = Integer.MIN_VALUE/2;
+    
+    for(int i = 1; i < n; i++) {
+      int res = explore(n, sum + i, dp);
+      max = Math.max(max, res * i);
+    }
+    dp[sum] = max;
+    return max;
+  }
+}
